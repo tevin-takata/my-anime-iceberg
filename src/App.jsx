a160166979d08f0a6b3eb1cae4e9e792d6eb4197
Tevin Takata <takatat9@hawaii.edu>
2024-07-04 01:21:09 -1000

    App UI finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Footer from './components/Footer'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1>MyAnimeIceberg</h1>
+        <h2 style={{ marginBottom: '2em' }}>How Deep Do Your Anime Preferences Go?</h2>
+        <button style={{ marginBottom: '5em'}}>
+          Log in with MyAnimeList
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer/>
     </>
   )
 }
-
-export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import '../App.css'
+
+export default function Footer() {
+  return (
+    <>
+      <div>
+        <p>
+          Made by <a href='https://tevin-takata.github.io/'>Tevin Takata</a>
+        </p>
+        <p>
+          Home | About | Privacy Policy
+        </p>
+      </div>
+    </>
+  )
+}
